f24547c9c7279c4c7f6122e4e08696ce3dd88b15
Thomas <Thomas@DESKTOP-A92MFOC.private.univ-tlse3.fr>
2025-09-29 18:55:09 +0200

    PrePost conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -7,6 +7,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified();
 	}
 
 	public String getNom() {
@@ -22,12 +23,23 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert forceCoup > 0;
+		int sauvForce = force;
 		force -= forceCoup;
 		if (force < 1) {
 			parler("J'abandonne !");
 		} else {
-			parler("Aïe");
+			parler("Aï¿½e");
 		}
+		assert force < sauvForce;
+		assert isInvariantVerified();
 	}
 
+	private boolean isInvariantVerified() {
+		return force >= 0;
+	}
+
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
+	}
 }
